20c38bb18d0e3a93210db742318405cd61147eb1
Felipe Maza <felipe@felipem.com>
2015-08-09 19:05:08 +0200

    Superamos el reto 01

---
+++ challenge01.js
@@ -0,0 +1,18 @@
+{
+    init: function(elevators, floors) {
+        var elevator = elevators[0]; // Let's use the first elevator
+
+
+        elevator.on("idle", function() {
+
+            elevator.goToFloor(0);
+            elevator.goToFloor(1);
+
+            // Añadimos que suba a la segunda planta.
+            elevator.goToFloor(2);
+        });
+    },
+    update: function(dt, elevators, floors) {
+        // We normally don't need to do anything here
+    }
+}
